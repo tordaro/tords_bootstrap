bfa4e1332bb905db14bd1ee09e7b57a434e6b6d6
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2024-03-10 00:04:45 +0100

    nvchad custom

---
+++ custom/init.lua
@@ -0,0 +1,14 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
+})
