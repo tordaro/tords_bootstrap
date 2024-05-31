6d94934459f5afd8d69aca8242367ac22fab1432
Tord Aronsen <tordaro@gmail.com>
2024-05-31 23:45:40 +0200

    use '' to jump to exact position

--- custom/init.lua
+++ custom/init.lua
@@ -16,3 +16,6 @@ autocmd("BufWritePre", {
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
+
+-- remap double single quote to jump to the beginning of the line of a mark
+vim.api.nvim_set_keymap("n", "''", "`", { noremap = true, silent = true })
