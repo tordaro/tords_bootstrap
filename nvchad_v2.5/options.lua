b37915887c08af7ebbec16a0bd00ed059cab4799
Tord Aronsen <tord.aronsen@skatteetaten.no>
2025-01-10 15:23:34 +0100

    new nvchad version

---
+++ nvchad_v2.5/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.number = true           -- Show the absolute number on the current line
+vim.opt.relativenumber = true   -- Enable relative numbering for other lines
+
